37d17ba13f7117bf481079de924dbdf3080917fc
dizr-odoo <dizr@odoo.com>
2023-07-18 15:43:35 -0600

    first

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Module to handle courses and sessions""",
+    'description': """Modulet o handle 
+        -Courses
+        -Session
+        -Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'dizr',
+    'website': 'www.odoo.com',
+    'category': 'Custom Modules/Technical Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
