4481cba92dbc3918cdac53178bdce2cc1c9f79a6
dizr-odoo <dizr@odoo.com>
2023-07-20 13:24:07 -0600

    Added relational fields

--- registry/models/course.py
+++ registry/models/course.py
@@ -19,6 +19,17 @@ class Course(models.Model):
     certificate_title = fields.Binary()
     register_date = fields.Date()
 
+    owner_id = fields.Many2one(comodel_name="res.partner", string="Owner")
+    owner_name = fields.Char(related="owner_id.name")
+    owner_email = fields.Char(related="owner_id.email")
+    owner_phone = fields.Char(related="owner_id.phone")
+
+    #computed fields
+    brand = fields.Char(string="Brand", compute="_compute_brand")
+    make = fields.Char(string="Make", compute="_compute_make")
+    model = fields.Char(string="Model", compute="_compute_model")
+
+
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
@@ -32,18 +43,35 @@ class Course(models.Model):
         vin_pattern = r'^[A-Z]{4}\d{2}[A-Z\d]{2}\d{6}$'
         for registry_number in self:
             if re.match(vin_pattern, registry_number.vin) == None:
-                raise ValidationError("""The VIN must follow the pattern
+                raise ValidationError(("""The VIN must follow the pattern
                                       Make - 2 Capital Letters
                                       Model - 2 Capital Letters
                                       Year - 2 Digits
                                       Battery Capacity - 2 Capital Letters or Numbers
-                                      Serial Number - 6 Digits """)
+                                      Serial Number - 6 Digits """))
+    
     @api.constrains('license_plat')
     def _check_license_plat(self):
         license_plat_pattern = r'^[A-Z]{1,4}\d{1,3}([A-Z]{2})?$'
         for registry_number in self:
             if re.match(license_plat_pattern, registry_number.license_plat) == None:
-                raise ValidationError("""The License Plate should follow the following Pattern:
+                raise ValidationError(("""The License Plate should follow the following Pattern:
                                       1 - 4 Capital Letters
                                       1 - 3 Digits
-                                      Optional 2 Capital Letters""")
+                                      Optional 2 Capital Letters"""))
+            
+    @api.depends("vin")
+    def _compute_brand(self):
+        for record in self:
+            if record.vin:
+                record.brand = record.vin[0:2]
+    @api.depends("vin")
+    def _compute_make(self):
+        for record in self:
+            if record.vin:
+                record.make = record.vin[2:4]
+    @api.depends("vin")
+    def _compute_model(self):
+        for record in self:
+            if record.vin:
+                record.model = record.vin[4:6]
