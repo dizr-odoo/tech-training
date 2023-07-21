718e6e5f640bf07134d9146dbb2de9606f3e85f6
dizr <dizr@odoo.com>
2023-07-21 11:13:49 -0600

    TE11 se agrega por herencia la page motorcycle info

--- registry/__manifest__.py
+++ registry/__manifest__.py
@@ -8,14 +8,15 @@ This Module is used to keep track of the Motorcycle Registration and Ownership o
     'author': 'dizr',
     'website': 'https://github.com/dizr-odoo/tech-training',
     'category': 'Kawiil/Kawiil',
-    'depends': ['base'],
+    'depends': ['stock'],
     'data': [
         'security/registry_groups.xml',
         'security/registry_security.xml',
         'security/ir.model.access.csv',
         'data/registry_data.xml',
         'views/registry_menuitems.xml',
-        'views/registry_views.xml'
+        'views/registry_views.xml',
+        'views/product_views_inheritance.xml'
     ],
     'demo': ['demo/course_demo.xml',
              ],
---
+++ registry/models/product.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class Product(models.Model):
+    _inherit = 'product.template'
+
+    horsepower = fields.Float()
+    top_speed = fields.Float()
+    torque = fields.Float()
+    battery_capacity = fields.Char()
+    charge_time = fields.Float()
+    range = fields.Float()
+    curb_weight = fields.Float()
+    make = fields.Char()
+    model = fields.Char()
+
+    detailed_type = fields.Selection(selection_add=[
+        ('motorcycle', 'Motorcycle'),
+        ], ondelete={'motorcycle': 'set product'})
+    
+
+    
+    def _detailed_type_mapping(self):
+        type_mapping = super()._detailed_type_mapping()
+        type_mapping['motorcycle'] = 'product'
+        return type_mapping
